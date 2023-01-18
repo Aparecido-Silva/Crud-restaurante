92f3d8847bb96c645d80b1b243817ad21c33e101
Aparecido Silva <aparecido.silva12y@gmail.com>
2023-01-18 16:28:02 -0300

    atualização

--- models/db.js
+++ models/db.js
@@ -13,3 +13,9 @@ const sequelize = new Sequelize('bd_login', 'root', '@Aparecido12', {
     });
 
   module.exports = sequelize;
+
+  function login() {
+    const nome = document.querySelector('#nome').value
+    const email = document.querySelector('#email').value
+    const senha = document.querySelector('#senha').value
+  }

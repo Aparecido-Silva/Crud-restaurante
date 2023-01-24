93a683a819a0538a22104d071b41dd47e9219700
Aparecido Silva <aparecido.silva12y@gmail.com>
2023-01-24 16:01:29 -0300

    Servidor carregando o Site de cadastro

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const serve = require('./serve');
+const server = require('./serve');
 const user = require('./models/user');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
--- serve.js
+++ serve.js
@@ -1,9 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express()
 const http = require('node:http');
-
-const port = 3000;
-
+const port = 2020;
 const server = http.createServer();
 
-server.listen(port,() => {
-    console.log("Deu certo!");
-});
+/* 
+app.listen(port,() => {
+    console.log("Servidor criado com sucesso!");
+});*/
+
+//Passando meus arquivos para o node
+app.use(express.static(path.join(__dirname,("Site-cadastro"))))
+
+
+//Rotas Servidor: http://localhost:2020/
+app.get("/", function (res, res) {
+    res.sendFile(__dirname + "/Site-cadastro/index.html");
+})
+
+
+module.exports = server;
